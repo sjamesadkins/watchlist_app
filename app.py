acf914bef64442dd26a40a00aa5680fd83e0fb08
SAM ADKINS <sjamesadkins@gmail.com>
2024-02-24 14:43:07 -0600

    created app and database

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 import database
 
 menu = """Please select one of the following options:
@@ -11,17 +12,36 @@ menu = """Please select one of the following options:
 Your selection: """
 welcome = "Welcome to the watchlist app!"
 
+def prompt_add_movie():
+    title = input("Movie title: ")
+    release_date = input("Release date (dd-mm-YYYY): ")
+    parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
+    timestamp = parsed_date.timestamp()
+
+    database.add_movie(title, timestamp)
+
+
+def print_movie_list(heading, movies):
+    print(f"-- {heading} movies --")
+    for movie in movies:
+        movie_date = datetime.datetime.fromtimestamp(movie[1])
+        human_date = movie_date.strftime("%b %d %Y")
+        print(f"{movie[0]} (on {human_date})")
+    print("---- \n")
+
 
 print(welcome)
 database.create_tables()
 
 while (user_input := input(menu)) != "6":
     if user_input == "1":
-        pass
+        prompt_add_movie()
     elif user_input == "2":
-        pass
+        movies = database.get_movies(True)
+        print_movie_list("Upcoming", movies)
     elif user_input == "3":
-        pass
+        movies = database.get_movies()
+        print_movie_list("All", movies)
     elif user_input == "4":
         pass
     elif user_input == "5":
